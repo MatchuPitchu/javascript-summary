6a071275d205ff257dd60a95e599549530947876
MatchuPitchu <michael.flohr@posteo.de>
2022-10-20 19:15:42 +0200

    feat: add example drag and drop

--- project-drag-and-drop/src/main.ts
+++ project-drag-and-drop/src/main.ts
@@ -1,62 +1,87 @@
 import './style.css';
 
-const list = document.querySelector<HTMLUListElement>('ul')!;
-const items = document.querySelectorAll<HTMLLIElement>('li')!;
+/*** Dragged item was clicked ***/
+// [1] set attribute draggable="true" to all items that should be draggable (-> HTML)
+// [2] save item that should be dragged
+// [3] define data type und attache data that you want to transfer to drag event
+// https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/Recommended_drag_types
+// [4] specify effect that is allowed for a drag operation
+// https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/effectAllowed
 
-/*** TO BE FINISHED: https://webdevtrick.com/html-drag-and-drop-list/ ****/
+/*** Dragged item enters valid drop target ***/
+// [5] preventDefault is mandatory, otherwise drag would be cancelled by default,
+// if my defined data type is NOT there, drag would be cancelled
+// [6] set visual feedback for user
 
-const initDraggable = () => {
+/*** Element is being dragged over a valid drop target ***/
+// [7] set also preventDefault()
+
+/*** Dragged element leaves a valid drop target ***/
+// [8] remove visual feedback for user
+
+/*** Element is dropped on a valid drop target ***/
+// [9] if dragged element is NOT drop target, then switch content between them
+
+/*** Drag operation is being ended ***/
+// [10] clear added stylings
+
+const initDragAndDrop = () => {
+  const list = document.querySelector<HTMLUListElement>('ul')!;
+  const items = document.querySelectorAll<HTMLLIElement>('li')!;
+
+  let dragSourceElement: HTMLElement;
+
   items.forEach((item) => {
     item.addEventListener('dragstart', (event) => {
       if (!event.dataTransfer) return;
-
-      const id = (event.target as HTMLLIElement).id;
-      // [1] define data type und attached data to drag event
-      // https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/Recommended_drag_types
-      event.dataTransfer.setData('text/plain', id);
-
-      // [2] specifies the effect that is allowed for a drag operation
-      // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/effectAllowed
+      // [2]
+      dragSourceElement = event.target as HTMLLIElement;
+      // [3]
+      event.dataTransfer.setData('text/plain', dragSourceElement.innerHTML);
+      // [4]
       event.dataTransfer.effectAllowed = 'move';
     });
 
-    item.addEventListener('dragend', (event) => {
-      console.log(event);
+    item.addEventListener('dragenter', (event) => {
+      // [5]
+      if (event.dataTransfer?.types[0] !== 'text/plain') return;
+      event.preventDefault();
+
+      // [6]
+      (event.currentTarget as HTMLElement).classList.add('over');
+    });
+
+    item.addEventListener('dragover', (event) => {
+      // [7]
+      if (event.dataTransfer?.types[0] !== 'text/plain') return;
+      event.preventDefault();
+      event.dataTransfer.dropEffect = 'move';
+    });
+
+    item.addEventListener('dragleave', () => {
+      // [8]
+      item.classList.remove('over');
+    });
+
+    item.addEventListener('drop', (event) => {
+      const targetContent = event.currentTarget as HTMLElement;
+      // [9]
+      if (dragSourceElement !== targetContent) {
+        dragSourceElement.innerHTML = targetContent.innerHTML;
+        targetContent.innerHTML = event.dataTransfer?.getData('text/plain') ?? '';
+      }
+
+      list.classList.remove('over');
+    });
+
+    item.addEventListener('dragend', () => {
+      // [10]
+      const items = document.querySelectorAll<HTMLLIElement>('li')!;
+      items.forEach((item) => {
+        item.classList.remove('over');
+      });
     });
   });
 };
 
-const initDroppable = () => {
-  list.addEventListener('dragenter', (event) => {
-    // [3] preventDefault is mandatory, otherwise drag would be cancelled by default
-    // so if my defined data type is NOT there, drag would be cancelled
-    if (event.dataTransfer?.types[0] !== 'text/plain') return;
-
-    event.preventDefault();
-    // [4] set visual feedback for user
-    list.classList.add('droppable');
-  });
-
-  list.addEventListener('dragover', (event) => {
-    if (event.dataTransfer?.types[0] !== 'text/plain') return;
-    event.preventDefault();
-  });
-
-  list.addEventListener('dragleave', (event) => {
-    // [4] remove visual feedback for user
-    // check if relatedTarget (area over which you move an dragged item) has an ancestor of your <ul>
-    // so class is only removed if you leave your wished droppable area
-    if ((event.relatedTarget as HTMLElement).closest('ul') !== list) {
-      list.classList.remove('droppable');
-    }
-  });
-
-  list.addEventListener('drop', (event) => {
-    const id = event.dataTransfer?.getData('text/plain');
-
-    list.classList.remove('droppable');
-  });
-};
-
-initDraggable();
-initDroppable();
+initDragAndDrop();
